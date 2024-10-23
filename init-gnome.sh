3839a6cfc252591de9f549b6cf449c3f6a7d22d8
Atilla <atillacolgecen@gmail.com>
2024-10-23 22:46:34 +0200

    Added input wallpaper

--- init-gnome.sh
+++ init-gnome.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Define the color variable
-wallpaper="orange"
-
 # Function to check command success
 check_command() {
     if [ $? -eq 0 ]; then
@@ -17,13 +14,26 @@ GREEN='\033[0;32m'  # Green color for success messages
 YELLOW='\033[1;33m' # Yellow color for informational messages
 NOCOLOR='\033[0m'   # No color / reset ANSI code
 
+# Ask the user to choose a wallpaper color
+echo -e "${YELLOW}Which color do you want for your wallpaper?${NOCOLOR}"
+echo -e "${YELLOW}Choose between red, orange, or blue:${NOCOLOR}"
+read -p "Enter colour: " input_colour
+
+# Ensure the input is valid
+if [[ "$input_colour" != "red" && "$input_colour" != "orange" && "$input_colour" != "blue" ]]; then
+    echo -e "${YELLOW}Invalid color choice. Defaulting to orange.${NOCOLOR}"
+    wallpaper="orange"
+else
+    wallpaper="$input_colour"
+fi
+
+echo -e "${YELLOW}Setting wallpaper color to ${wallpaper}${NOCOLOR}"
+
 # Disable home folder icon on the desktop
 echo -e "${YELLOW}Disabling home folder${NOCOLOR}"
 gsettings set org.gnome.shell.extensions.ding show-home false
 check_command
 echo ""
-
-
 
 # Add battery percentage to the top panel
 echo -e "${YELLOW}Adding top panel battery percentage${NOCOLOR}"
@@ -31,15 +41,11 @@ gsettings set org.gnome.desktop.interface show-battery-percentage true
 check_command
 echo ""
 
-
-
 # Disable date in the top panel clock
 echo -e "${YELLOW}Disabling top panel date${NOCOLOR}"
 gsettings set org.gnome.desktop.interface clock-show-date false
 check_command
 echo ""
-
-
 
 # Add week number to the calendar
 echo -e "${YELLOW}Adding weekdate to calendar${NOCOLOR}"
@@ -47,15 +53,11 @@ gsettings set org.gnome.desktop.calendar show-weekdate true
 check_command
 echo ""
 
-
-
 # Set Tilix as the default terminal
 echo -e "${YELLOW}Tilix as default terminal${NOCOLOR}"
 gsettings set org.gnome.desktop.default-applications.terminal exec 'warp-terminal'
 check_command
 echo ""
-
-
 
 # Set dark mode theme
 echo -e "${YELLOW}Setting dark mode${NOCOLOR}"
@@ -63,15 +65,11 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-viridian-dark'
 check_command
 echo ""
 
-
-
 # Set Viridian icon theme
 echo -e "${YELLOW}Setting viridian mode${NOCOLOR}"
 gsettings set org.gnome.desktop.interface icon-theme 'Yaru-viridian'
 check_command
 echo ""
-
-
 
 # Move the "Show Applications" button to the top of the dock
 echo -e "${YELLOW}Setting show applications to right${NOCOLOR}"
@@ -79,23 +77,17 @@ gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 check_command
 echo ""
 
-
-
 # Set the dock position to the bottom
 echo -e "${YELLOW}Setting dock position to bottom${NOCOLOR}"
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
 check_command
 echo ""
 
-
-
 # Set the dock icon size (Edit to wish)
 echo -e "${YELLOW}Setting dock icon size${NOCOLOR}"
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 28
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
 check_command
 echo ""
-
-
 
 # Enable dock autohide
 echo -e "${YELLOW}Setting dock autohide${NOCOLOR}"
@@ -104,15 +96,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
 check_command
 echo ""
 
-
-
 # Set dock transparency mode to fixed
 echo -e "${YELLOW}Setting dock transparency to fixed${NOCOLOR}"
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
 check_command
 echo ""
-
-
 
 # Set dock background opacity (Edit to wish)
 echo -e "${YELLOW}Setting dock background opacity${NOCOLOR}"
@@ -120,15 +108,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.1
 check_command
 echo ""
 
-
-
 # Hide the trash icon from the dock
 echo -e "${YELLOW}Setting dock show trash to false${NOCOLOR}"
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 check_command
 echo ""
-
-
 
 # Hide mounted drives from the dock
 echo -e "${YELLOW}Setting dock show mounts to false${NOCOLOR}"
@@ -136,15 +120,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 check_command
 echo ""
 
-
-
 # Set dock running indicator color to dominant color
 echo -e "${YELLOW}Setting dock running indicator dominant color${NOCOLOR}"
 gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color true
 check_command
 echo ""
-
-
 
 # Set dock panel mode to extend height
 echo -e "${YELLOW}Setting dock panel mode${NOCOLOR}"
@@ -152,15 +132,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 check_command
 echo ""
 
-
-
 # Disable dynamic workspaces
 echo -e "${YELLOW}Setting workspace to false${NOCOLOR}"
 gsettings set org.gnome.mutter dynamic-workspaces false
 check_command
 echo ""
-
-
 
 # Set the number of workspaces to 1
 echo -e "${YELLOW}Setting workspace to fixed number${NOCOLOR}"
@@ -168,23 +144,15 @@ gsettings set org.gnome.desktop.wm.preferences num-workspaces 1
 check_command
 echo ""
 
-
-
 # Set keyboard shortcut for opening the Files application
 echo -e "${YELLOW}Setting Files keyboard shortcut${NOCOLOR}"
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>E']"
 check_command
 echo ""
 
-
-
-# Download and set wallpaper
+# Download and set wallpaper based on the selected color
 mkdir -p ~/Pictures/Wallpapers/
-curl -o ~/Pictures/Wallpapers/ubuntu_wallpaper.jpg https://raw.githubusercontent.com/acolgecen/wallpaper/main/ubuntu/ubuntu_${wallpaper}.jpg
+curl -o ~/Pictures/Wallpapers/ubuntu_wallpaper.jpg "https://raw.githubusercontent.com/acolgecen/wallpaper/main/ubuntu/ubuntu_${wallpaper}.jpg"
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/Wallpapers/ubuntu_wallpaper.jpg"
 check_command
 echo ""
-
-
-
-# echo -e "${YELLOW}All changes applied successfully${NOCOLOR}"
